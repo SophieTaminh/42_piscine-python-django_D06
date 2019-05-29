22528a09e8c2c73e2ef002e29e907e52214fccdc
staminhe42 <staminhe42@gmail.com>
2019-05-29 22:46:57 +0200

    redirection titre ex01

--- d06/ex/urls.py
+++ d06/ex/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.accueil, name='accueil'),
     re_path(r'^$', views.accueil, name='accueil'),
+    re_path('accueil', views.accueil, name='accueil'),
     path('inscription', views.inscription, name='inscription'),
    	path('connexion', views.connexion, name='connexion'),
 ]
