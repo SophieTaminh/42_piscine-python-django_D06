2adb301c4909a99993776607cc32c63ed2aee0ed
staminhe42 <staminhe42@gmail.com>
2019-05-29 23:40:24 +0200

    navigation

--- d06/ex/urls.py
+++ d06/ex/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.accueil, name='accueil'),
     re_path(r'^$', views.accueil, name='accueil'),
-    re_path('accueil', views.accueil, name='accueil'),
     path('inscription', views.inscription, name='inscription'),
    	path('connexion', views.connexion, name='connexion'),
 ]
